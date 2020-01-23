9af9795bb1d14ae09b74b98064df063f1632093a
Esteban <ecristobalr@gmail.com>
2020-01-23 13:39:27 +0100

    Created project structure

---
+++ src/main/java/es/ecristobal/cassiopeia/server/CassiopeiaServerApplication.java
@@ -0,0 +1,13 @@
+package es.ecristobal.cassiopeia.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CassiopeiaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CassiopeiaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/es/ecristobal/cassiopeia/server/CassiopeiaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package es.ecristobal.cassiopeia.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CassiopeiaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
